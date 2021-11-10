f471933c1d5c4312935f674dfa462768941e57d3
yas <yas@17536.no-reply.drupal.org>
2021-11-10 13:46:23 -0800

    Refactored

--- INSTALL.sh
+++ INSTALL.sh
@@ -45,7 +45,7 @@ sudo apt -y install python3-pip python3-bluez python3-yaml python3-boto3 python3
 
 echo
 export CLIENT_SECRETS="$(pwd)/client_secrets.json"
-echo_count "Creating client_secrets.json: ${CLIENT_SECRETS}"
+echo_count "Creating client_secrets.json '${CLIENT_SECRETS}'..."
 sudo cat << CLIENT_SECRETS > "${CLIENT_SECRETS}"
 {"installed":{"client_id":"${GAPPS_CLIENT_ID}","project_id":"${GAPPS_PROJECT_ID}","auth_uri":"https://accounts.google.com/o/oauth2/auth","token_uri":"https://accounts.google.com/o/oauth2/token","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_secret":"${GAPPS_CLIENT_SECRET}","redirect_uris":["urn:ietf:wg:oauth:2.0:oob","http://localhost"]}}
 CLIENT_SECRETS
